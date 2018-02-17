c86d173d5f1d3ef0be08da8b0f22956124df00fe
j0e009t <jose.escobar1@walmart.com>
2018-02-17 11:09:59 -0600

    - Grid layout with a grid background img - Letter O images in every square of the grid background img

---
+++ app/src/main/java/com/escobar/j0e009t/tictactoe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.escobar.j0e009t.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
